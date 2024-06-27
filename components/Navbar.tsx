32a5b1e3c8054fb280bc52e6e46732ff1f8cf73e
Arek <arek.adamowicz81@gmail.com>
2024-06-27 18:10:28 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <nav className="max-container padding-container flexBetween relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 flexCenter cursor-pointer pb-1.5 text-gray-50 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
